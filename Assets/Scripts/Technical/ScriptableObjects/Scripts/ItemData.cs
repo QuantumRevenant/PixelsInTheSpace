942148d8bef7ee67e41d7f1edfab529d233694d4
QuantumRevenant <sebaperales14@gmail.com>
2022-12-14 01:04:17 -0500

    Add Proto LootTable and Proto PowerUp Data Scr Obj Add Proto Loot Table with Scriptable Object(No functionality) Add Proto Item Data with Scriptable Object(No functionality)

---
+++ Assets/Scripts/Technical/ScriptableObjects/Scripts/ItemData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+[CreateAssetMenu(fileName ="New Item Data", menuName ="Scriptable Objects/Item Data")]
+public class ItemData : ScriptableObject
+{
+    [SerializeField]private Sprite itemSprite;
+    [SerializeField]private bool isActivable=true;
+    [SerializeField]private Items itemClass;
+    [SerializeField]private bool isTemporal=false;
+    [SerializeField]private float multiplicatorValue=0f;
+    [SerializeField]private float itemSpeed=0.5f;
+}
