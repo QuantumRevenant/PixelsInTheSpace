f1c5fc1f213788e2ae863f3421962d550c0a9895
QuantumRevenant <94991329+QuantumRevenant@users.noreply.github.com>
2024-04-26 12:36:37 -0500

    refactor: :fire: Deleting unuseable files from Archive/

--- Assets/Code/Scripts/Archive/2022 and before/ScriptableObjects/Scripts/ShootingTableData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(fileName = "New Shooting Table Data", menuName = "Scriptable Objects/Shooting Table Data")]
-public class ShootingTableData : ScriptableObject
-{
-    [SerializeField]private ShootingData[] _shootingData=new ShootingData[1];
-    public ShootingData[] shootingData { get { return _shootingData; } }
-}
